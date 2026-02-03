8fae3a0c07c1775d69a7b6ab786a9797440312cd
heythere-jpg <denisnenitoilet@gmail.com>
2026-02-03 14:26:19 +0100

    Update version.js

--- api/version.js
+++ api/version.js
@@ -1,7 +1,7 @@
 // api/version.js
 export default function handler(req, res) {
   // Version number (change this to update the game version)
-  const version = "V0.26.stone.2";
+  const version = "V0.26.stone.3";
 
   // Optional: can add custom messages or extra fields
   res.status(200).json({ version });
